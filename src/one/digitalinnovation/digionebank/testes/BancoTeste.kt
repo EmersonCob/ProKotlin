49087866835830de858c0f01f1e735151c0fedd9
EmersonCob <58399211+EmersonCob@users.noreply.github.com>
2021-03-06 20:56:32 -0300

    created bank class and test to it

---
+++ src/one/digitalinnovation/digionebank/testes/BancoTeste.kt
@@ -0,0 +1,10 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Banco
+
+fun main(){
+    val digiOneBabk = Banco( name = "DigiOne", numero = 12)
+
+    println(digiOneBabk.name)
+    println(digiOneBabk.numero)
+}
