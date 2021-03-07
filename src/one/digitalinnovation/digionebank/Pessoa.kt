06054b3af921a0803de1e6502e943f53b00f7f9d
EmersonCob <58399211+EmersonCob@users.noreply.github.com>
2021-03-06 22:49:51 -0300

    Update to make person and employer abstract class

--- src/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,19 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var name: String = "José"
-
-    var cpf: String = "123.123.123-00"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$name e $cpf"
-
-}
-
-fun main(){
-    val jose = Pessoa()
-
-    println(jose.pessoaInfo())
-}
+abstract class Pessoa(
+    val name: String,
+    val cpf: String
+)
